33164c0e4304489404ea497df7f5854f0d40c60f
polarker <polarker@users.noreply.github.com>
2024-03-11 20:41:13 +0000

    deploy: 10d8019250957beb30d0a42860b10ae75ed14061

---
+++ _next/static/EgfFg7WtgpyJIVTqIF--M/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/885-60381dcf45ad03b4.js","static/css/5aae1f7c67fbffd5.css","static/chunks/pages/index-abbdf409d80abd7e.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
